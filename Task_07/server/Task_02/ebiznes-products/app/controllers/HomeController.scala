1344b66be763d3256d903f1818c04e3d95c5e4fc
Jacek Golebiowski <jacek-golebiowski@users.noreply.github.com>
2025-05-07 11:20:12 +0200

    test bad formatting

--- Task_07/server/Task_02/ebiznes-products/app/controllers/HomeController.scala
+++ Task_07/server/Task_02/ebiznes-products/app/controllers/HomeController.scala
@@ -21,4 +21,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
+  def badFormatting  =  println("test")
+
 }
