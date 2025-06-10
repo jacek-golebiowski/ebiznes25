bbcd2d027d19252374e67f5a148da8a07be84d09
Jacek Golebiowski <jacek-golebiowski@users.noreply.github.com>
2025-06-10 19:02:52 +0200

    small fix

--- Task_10/backend/src/main/java/com/example/demo/DemoApplication.java
+++ Task_10/backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,13 @@
+package com.example.demo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class,
+    org.springframework.boot.autoconfigure.data.r2dbc.R2dbcDataAutoConfiguration.class
+})
 @RestController
 public class DemoApplication {
   public static void main(String[] args) {
